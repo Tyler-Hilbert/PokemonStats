2b3e38c7ada0eac55fe2e6c690e0213c996a8c17
tyler-hilbert <tylerjoshuahilbert@gmail.com>
2017-04-06 23:37:03 -0400

    basic stats

---
+++ PokemonStats.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import pylab
+
+pokemon_df = pd.read_csv("pokemon.csv")
+
+print(pokemon_df['Type_1'].value_counts() + pokemon_df['Type_2'].value_counts())
+
+stats = pokemon_df.groupby('Type_1').mean() + pokemon_df.groupby('Type_2').mean()
+
+print (stats.hist(bins=15))
+pylab.show()
