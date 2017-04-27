0dcb00e5b699045d4702a07edfb7358995787721
tyler-hilbert <tylerjoshuahilbert@gmail.com>
2017-04-26 21:31:56 -0400

    Loop through each type

--- PokemonStats.py
+++ PokemonStats.py
@@ -9,7 +9,7 @@ pokemon_df = pd.read_csv("pokemon.csv")
 # Break pokemon of 2 types into 2 entries
 stat_cols = ['HP', 'Attack', 'Defense', 'Sp_Atk', 'Sp_Def','Speed', 'Catch_Rate', 'Body_Style']
 type_cols = ['Type_1', 'Type_2']
-melted = pd.melt(pokemon_df, stat_cols, type_cols, value_name='Type')
+melted = pd.melt(pokemon_df, stat_cols, type_cols, value_name='Type').dropna()
 
 # Describe types
 described_stats = melted.groupby('Type')[stat_cols].describe()
@@ -29,27 +29,22 @@ print(pokemon_df['Body_Style'].value_counts())
 
 
 
-# Find highest HP
-#types = set(melted["Type"])
-#for type_ in types:
-#	print(melted["HP"][melted["Type"]==type])
-#	print(melted["HP"][melted["Type"]==type].mean())
-
-
 
 # Print
 print("\nGraphing\n")
 
-specific_cols = ['HP', 'Attack', 'Defense', 'Sp_Atk', 'Sp_Def','Speed']
-stats = melted[specific_cols][melted['Type']=="Fire"].mean()
+types = set(melted["Type"])
+for type in types:
+	specific_cols = ['HP', 'Attack', 'Defense', 'Sp_Atk', 'Sp_Def','Speed']
+	stats = melted[specific_cols][melted['Type']==type].mean()
 
-objects = ('HP', 'Attack', 'Defense', 'Sp_Atk', 'Sp_Def','Speed') # TODO find a way to convert this without retyping
-y_pos = np.arange(len(objects))
-#performance = [10,8,6,4,2,1]
- 
-plt.bar(y_pos, stats, align='center', alpha=0.5)
-plt.xticks(y_pos, stat_cols)
-plt.ylabel('Base')
-plt.title('Fire stats')
- 
-plt.show()
+	objects = ('HP', 'Attack', 'Defense', 'Sp_Atk', 'Sp_Def','Speed') # TODO find a way to convert this without retyping
+	y_pos = np.arange(len(objects))
+
+	plt.bar(y_pos, stats, align='center', alpha=0.5)
+	plt.xticks(y_pos, stat_cols)
+	plt.ylabel('Base')
+	title = type + ' stats'
+	plt.title(title)
+	 
+	plt.show()
